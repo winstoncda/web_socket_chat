1ded20dd01ed0c89b1cbd4548ffbec3da37c7ff3
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 16:23:25 +0200

    création du controller et de la route pour envoyer un message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersforSidebar = async (req, res) => {
   try {
@@ -29,5 +30,32 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log("error in get all messages", error);
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverUserId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId: receiverUserId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("error in send a message", error);
   }
 };
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUsersforSidebar,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,7 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersforSidebar);
 
 router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
