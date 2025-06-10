648e5cc6d3663ee8a3f7f7b190138de606bb89a9
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 15:19:22 +0200

    création du middleware pour vérifier l'authentification avec le token

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,35 @@
+import jsonwebtoken from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookie.jwt;
+
+    if (!token) {
+      return res.status(400).json({
+        message: "Interdit ! Pas de token",
+      });
+    }
+
+    const decodedToken = jsonwebtoken.verify(token, process.env.SECRET_KEY);
+
+    if (!decodedToken) {
+      return res.status(400).json({
+        message: "Interdit ! Token invalide",
+      });
+    }
+
+    const user = await User.findById(decodedToken.userId).select("-password");
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Utilisateur inconnu",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("error in protect route middleware", error);
+  }
+};
