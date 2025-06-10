5eea9e29a925b8293d02f5f5fffc9a1ab5ebf386
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 14:47:15 +0200

    création du controller logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -80,4 +80,15 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+    });
+    res.status(200).json({
+      message: "Déconnexion réussie",
+    });
+  } catch (error) {
+    console.log("Error in logout controller", error);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
