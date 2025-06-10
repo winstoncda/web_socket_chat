86e1cc40aa91ff09a34f9fbba11bd50e2c63d5f8
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 15:40:51 +0200

    création du controller pour la mise à jour du profil

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -93,4 +94,31 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profileAvatar } = req.body;
+    const userId = req.user._id;
+
+    if (!profileAvatar) {
+      return res.status(400).json({
+        message: "L'avatar est requis",
+      });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profileAvatar);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profileAvatar: uploadResponse.secure_url,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile", error);
+  }
+};
