a7fbd436268df9ce74e3396ddd8469ada9e62c43
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 13:39:17 +0200

    mise en place de la configuration pour les variables d'environnement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
