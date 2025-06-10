3432b0f7c318be4876c48740323e1aaf0ce67be4
winstoncda <olivier.pro.cda@gmail.com>
2025-06-10 13:45:17 +0200

    mise en place connexion à la base de données

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
