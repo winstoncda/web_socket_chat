39c7c394d40675f3a38b14dd45f14f682887811f
winstoncda <olivier.pro.cda@gmail.com>
2025-06-11 13:50:29 +0200

    mise en place de cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import cors from "cors";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -11,6 +13,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT;
 
