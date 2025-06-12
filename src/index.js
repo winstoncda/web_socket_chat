442456b4a13e753604f0e178404bfcdbf1150406
winstoncda <olivier.pro.cda@gmail.com>
2025-06-12 09:50:00 +0200

    configuration web socket

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { app, serverHttp } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -26,7 +26,7 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+serverHttp.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
