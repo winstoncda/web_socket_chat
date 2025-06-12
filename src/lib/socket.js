10dda9b28014ed140d14f932beed09d750faf79c
winstoncda <olivier.pro.cda@gmail.com>
2025-06-12 10:41:49 +0200

    réupération des id des utilisateurs connectés et renvoi vers client

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import { log } from "console";
 
 const app = express();
 const serverHttp = http.createServer(app);
@@ -11,8 +12,17 @@ const io = new Server(serverHttp, {
   },
 });
 
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
   console.log("Un utilisateur s'est connecté", socket.id);
+
+  console.log(userSocketMap);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("Un utilisateur s'est déconnecté", socket.id);
